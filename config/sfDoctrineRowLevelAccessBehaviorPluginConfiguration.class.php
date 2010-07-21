ecf1de2fb3968a8a27c9f605db436d9d61167f5f
Susan Lau <susan.lau@gmx.de>
2010-07-21 09:09:13 +0000

    enhance README with examples and remove extra example file

---
+++ config/sfDoctrineRowLevelAccessBehaviorPluginConfiguration.class.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the sperian package
+ * (c) 2009 Susan Lau <susan.lau@abados.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfDoctrineRowLevelAccessBehaviorPlugin configuration.
+ * 
+ * @package     sfDoctrineRowLevelAccessBehaviorPlugin
+ * @subpackage  config
+ * @author      Susan Lau <susan.lau@abados.de>
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfDoctrineRowLevelAccessBehaviorPluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
